2a0db7e0ce00cce2b3bec8b4d833a05e497d9a83
Yosuke Suzuki <yosuke.suzuki@gmail.com>
2015-01-12 20:06:26 +0900

    add blobstore controllers/models

---
+++ controllers/blobstoreimage.go
@@ -0,0 +1,196 @@
+// EXAMPLE FROM: https://github.com/GoogleCloudPlatform/appengine-angular-gotodos
+//
+// Copyright 2013 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+// gotodos is an App Engine JSON backend for managing a todo list.
+//
+// It supports the following commands:
+//
+// - Create a new todo
+// POST /todos
+// > {"text": "do this"}
+// < {"id": 1, "text": "do this", "created": 1356724843.0, "done": false}
+//
+// - Update an existing todo
+// POST /todos
+// > {"id": 1, "text": "do this", "created": 1356724843.0, "done": true}
+// < {"id": 1, "text": "do this", "created": 1356724843.0, "done": true}
+//
+// - List existing todos:
+// GET /todos
+// >
+// < [{"id": 1, "text": "do this", "created": 1356724843.0, "done": true},
+//    {"id": 2, "text": "do that", "created": 1356724849.0, "done": false}]
+//
+// - Delete 'done' todos:
+// DELETE /todos
+// >
+// <
+
+package controllers
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"models"
+
+	"appengine"
+	"appengine/blobstore"
+	"appengine/datastore"
+	"appengine/image"
+
+	"github.com/astaxie/beegae"
+)
+
+type BlobStoreImageController struct {
+	beegae.Controller
+}
+
+func (this *BlobStoreImageController) UploadURL() {
+	uploadURL, err := blobstore.UploadURL(this.AppEngineCtx, "/admin/blobstoreimage/handler", nil)
+	if err != nil {
+		this.Data["json"] = err
+		return
+	}
+	listDataSet := map[string]interface{}{"uploadurl": uploadURL.Path}
+	this.Data["json"] = listDataSet
+}
+
+func (this *BlobStoreImageController) Handler() {
+	q := datastore.NewQuery("__BlobInfo__").Order("-creation").Limit(100)
+	t := q.Run(this.AppEngineCtx)
+	for {
+		var bi blobstore.BlobInfo
+		key, err := t.Next(&bi)
+		if err == datastore.Done {
+			break
+		}
+		if err != nil {
+			break
+		}
+		log.Println(appengine.BlobKey(key.StringID()))
+		var imageOptions image.ServingURLOptions
+		imageURL, err := image.ServingURL(this.AppEngineCtx, appengine.BlobKey(key.StringID()), &imageOptions)
+		if err != nil {
+			log.Println("cannot get ServingURL")
+			this.Data["json"] = err
+			return
+		}
+		var blobstoreimage models.BlobStoreImage
+		blobstoreimage.Title = bi.Filename
+		imageURLString := "//" + imageURL.Host + imageURL.Path
+		blobstoreimage.BlobKey = string(key.StringID())
+		blobstoreimage.ImageURL = imageURLString
+		blobstoreimageExists := blobstoreimage.Exists(this.AppEngineCtx)
+		if blobstoreimageExists == false {
+			_, err = blobstoreimage.Create(this.AppEngineCtx)
+			if err != nil {
+				log.Println("cannot create entity")
+				this.Data["json"] = err
+				return
+			}
+
+		}
+	}
+	this.Data["json"] = map[string]string{"status": "done"}
+}
+
+func (this *BlobStoreImageController) Get() {
+	blobstoreimage := []models.BlobStoreImage{}
+	_, err := datastore.NewQuery("BlobStoreImage").Order("-UpdatedAt").GetAll(this.AppEngineCtx, &blobstoreimage)
+	if err != nil {
+		this.Data["json"] = err
+		return
+	}
+	listDataSet := map[string]interface{}{"items": blobstoreimage}
+	this.Data["json"] = listDataSet
+}
+
+func (this *BlobStoreImageController) Post() {
+	blobstoreimage, err := decodeBlobStoreImage(this.Ctx.Input.Request.Body)
+	if err != nil {
+		log.Println("decode err")
+		this.Data["json"] = err
+		return
+	}
+	b, err := blobstoreimage.Create(this.AppEngineCtx)
+	if err != nil {
+		this.Data["json"] = err
+	} else {
+		this.Data["json"] = &b
+	}
+}
+
+func (this *BlobStoreImageController) GetEntity() {
+	keyName := this.Ctx.Input.Param(":key_name")
+	key := datastore.NewKey(this.AppEngineCtx, "BlobStoreImage", keyName, 0, nil)
+	var blobstoreimage models.BlobStoreImage
+	err := datastore.Get(this.AppEngineCtx, key, &blobstoreimage)
+	if err != nil {
+		this.Data["json"] = err
+		return
+	} else {
+		this.Data["json"] = &blobstoreimage
+	}
+}
+func (this *BlobStoreImageController) UpdateEntity() {
+	keyName := this.Ctx.Input.Param(":key_name")
+	key := datastore.NewKey(this.AppEngineCtx, "BlobStoreImage", keyName, 0, nil)
+	var blobstoreimage models.BlobStoreImage
+	err := datastore.Get(this.AppEngineCtx, key, &blobstoreimage)
+	if err != nil {
+		this.Data["json"] = err
+		return
+	}
+	err = json.NewDecoder(this.Ctx.Input.Request.Body).Decode(&blobstoreimage)
+	if err != nil {
+		this.Data["json"] = err
+		return
+	}
+	b, err := blobstoreimage.Update(this.AppEngineCtx)
+	if err != nil {
+		this.Data["json"] = err
+	} else {
+		this.Data["json"] = &b
+	}
+}
+
+func (this *BlobStoreImageController) DeleteEntity() {
+	keyName := this.Ctx.Input.Param(":key_name")
+	key := datastore.NewKey(this.AppEngineCtx, "BlobStoreImage", keyName, 0, nil)
+	err := datastore.Delete(this.AppEngineCtx, key)
+	if err == nil {
+		this.Data["json"] = nil
+	} else {
+		this.Data["json"] = err
+	}
+}
+
+func (this *BlobStoreImageController) Render() error {
+	if _, ok := this.Data["json"].(error); ok {
+		this.AppEngineCtx.Errorf("blobstoreimag error: %v", this.Data["json"])
+	}
+	this.ServeJson()
+	return nil
+}
+
+func decodeBlobStoreImage(r io.ReadCloser) (*models.BlobStoreImage, error) {
+	defer r.Close()
+	var blobstoreimage models.BlobStoreImage
+	err := json.NewDecoder(r).Decode(&blobstoreimage)
+	return &blobstoreimage, err
+}
---
+++ models/blobstoreimage.go
@@ -0,0 +1,54 @@
+package models
+
+import (
+	"time"
+
+	"appengine"
+	"appengine/datastore"
+)
+
+// IOSAPP is a kind which store information around iOS App
+type BlobStoreImage struct {
+	KeyName   string    `json:"key_name" datastore:"KeyName"`
+	Title     string    `json:"title" datastore:"Tite"`
+	Note      string    `json:"note" datastore:"Note"`
+	BlobKey   string    `json:"blob_key" datastore:"BlobKey"`
+	ImageURL  string    `json:"image_url" datastore:"ImageURL"`
+	UpdatedAt time.Time `json:"updated_at" datastore:"UpdatedAt"`
+	CreatedAt time.Time `json:"created_at" datastore:"CreatedAt"`
+}
+
+func (b *BlobStoreImage) key(c appengine.Context) *datastore.Key {
+	return datastore.NewKey(c, "BlobStoreImage", b.KeyName, 0, nil)
+}
+
+func (b *BlobStoreImage) Exists(c appengine.Context) bool {
+	b.KeyName = b.BlobKey
+	var blobstoreimage BlobStoreImage
+	err := datastore.Get(c, b.key(c), &blobstoreimage)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func (b *BlobStoreImage) Create(c appengine.Context) (*BlobStoreImage, error) {
+	b.KeyName = b.BlobKey
+	// b.Title = b.BlobKey
+	b.CreatedAt = time.Now()
+	b.UpdatedAt = time.Now()
+	_, err := datastore.Put(c, b.key(c), b)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func (b *BlobStoreImage) Update(c appengine.Context) (*BlobStoreImage, error) {
+	b.UpdatedAt = time.Now()
+	_, err := datastore.Put(c, b.key(c), b)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
